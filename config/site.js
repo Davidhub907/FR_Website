b0d940600b17a88ded07a0bd454af287d5335264
David Gray <davidagray2005@gmail.com>
2026-07-10 16:30:47 -0800

    New file to hold all constants:

---
+++ config/site.js
@@ -0,0 +1,19 @@
+export const siteConfig = {
+  name: "Frontier Restoration LLC",
+
+  phone: {
+    display: "(907) 987-2916",
+    href: "tel:+19079872916",
+  },
+
+  email: {
+    address: "office@frontierrestorationllc.com",
+    gmailHref:
+      "https://mail.google.com/mail/?view=cm&fs=1&to=office%40frontierrestorationllc.com",
+  },
+
+  location: {
+    address: "2155 Frank Ave, Fairbanks, AK 99701",
+    serviceArea: "Fairbanks and surrounding communities",
+  },
+};
