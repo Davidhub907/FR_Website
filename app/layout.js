eccaf7afc166083b6c9d6faaf17585b748160029
David Gray <davidagray2005@gmail.com>
2026-07-10 16:10:11 -0800

    moved the header, nav, and footer into layout so they will apear on every page

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,10 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import Header from "@/components/Header";
+import NavigationBar from "@/components/NavigationBar";
+import Footer from "@/components/Footer";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -22,7 +26,14 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="flex min-h-full flex-col">
+        <Header />
+        <NavigationBar />
+
+        <main className="flex-1">{children}</main>
+
+        <Footer />
+      </body>
     </html>
   );
 }
