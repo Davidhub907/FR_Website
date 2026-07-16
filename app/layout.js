3018097e698c1fb3382b7f6661fdc27da904084f
David Gray <davidagray2005@gmail.com>
2026-07-16 11:05:02 -0800

    Changed the layout section

--- app/layout.js
+++ app/layout.js
@@ -5,8 +5,6 @@ import Header from "@/components/layout/Header";
 import NavigationBar from "@/components/layout/NavigationBar";
 import Footer from "@/components/layout/Footer";
 import WhyChooseUs from "@/components/layout/WhyChooseUs";
-import EmergencyCTA from "@/components/layout/EmergencyCTA";
-import CommonQuestions from "@/components/layout/CommonQuestions";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,9 +32,7 @@ export default function RootLayout({ children }) {
         <NavigationBar />
 
         <main className="flex-1">{children}</main>
-        <EmergencyCTA />
         <WhyChooseUs />
-        <CommonQuestions />
         <Footer />
       </body>
     </html>
