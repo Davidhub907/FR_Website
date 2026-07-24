60e048fc0032c79b2bf0507dc4073eee161b9db2
David Gray <davidagray2005@gmail.com>
2026-07-23 19:43:46 -0800

    Added comments to the constants file

--- config/site.js
+++ config/site.js
@@ -1,17 +1,23 @@
+// These are the constants of the website (Change the info here, it changes all over the site.)
+
+// This is the name of the business
 export const siteConfig = {
   name: "Frontier Restoration LLC",
 
+  // This is the phone number of the business, display is what shows, href is what number is called
   phone: {
     display: "(907) 987-2916",
     href: "tel:+19079872916",
   },
 
+  // This is the email for the site. Address is the display name, gmailHref is the email you go to when pressing the email button
   email: {
     address: "office@frontierrestorationllc.com",
     gmailHref:
       "https://mail.google.com/mail/?view=cm&fs=1&to=office%40frontierrestorationllc.com",
   },
 
+  // This is the location and service area of the business
   location: {
     address: "2155 Frank Ave, Fairbanks, AK 99701",
     serviceArea: "Fairbanks and surrounding communities",
