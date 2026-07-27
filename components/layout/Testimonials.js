dd12c3f7b5964c86103bb3ebf1ed904c4aa14772
David Gray <davidagray2005@gmail.com>
2026-07-27 14:12:31 -0800

    Moved the testimonials info into a data file for easier managment

--- components/layout/Testimonials.js
+++ components/layout/Testimonials.js
@@ -1,36 +1,10 @@
 import Image from "next/image";
+import { testimonials } from "@/data/testimonials";
 
 // Testimonials
 
 const googleReviewsUrl =
   "https://www.google.com/search?q=water+damage+restoration+fairbanks&sca_esv=1bfee9744c1a1100&sxsrf=APpeQnvHFp7fXKJeaW6KG3N6hmjgD6yegA:1785136976534&udm=1&lsack=UAdnaqubILiq0PEPo8ONoQc&sa=X&ved=2ahUKEwirg_K6qfKVAxU4FTQIHaNhI3QQjGp6BAg0EAA&biw=1241&bih=1298&dpr=1.5#sv=CAESzQEKuQEStgEKd0FKaVQ0dEtNcHZNQlExX2JibWs4aTZlTjlxMGRiZGhVVTBEa1BreEMtQnJJNDZ1S1lZUlhjaXFObU5UbzdvNG5SeXNlMEh6MjIyYlJUcVBfSFZ3VWFSeTk0VGU2bjBHZE1HWmtmdVkwQkVVQ2FxdVUxX3hzVzdVEhdvZ2RuYXZIMEd0elkwUEVQeWZtNzRBOBoiQURzcjlmUjIycFJBX2ZiLVR2d2tYSzUxNmJLQWQyLVFFQRIEODA1MRoBMyoAMAA4AUAAGAAg1anZ9AY6AEoCEAI";
-
-const testimonials = [
-  {
-    id: 1,
-    quote:
-      "Frontier Restoration came out to our place for a Mold remediation, and they did an amazing job! They also helped us bag up all our clothes and hangers, which was such a huge help. Josh was very friendly and professional, and his worker Bodie was wonderful too. They knocked it out in 30 mins! I highly recommend Frontier Restoration to anyone who needs help with mold cleanup, they made a stressful situation so much easier!",
-    name: "Kelsey",
-    location: "Fairbanks, Alaska",
-    service: "Mold inspection & remediation",
-  },
-  {
-    id: 2,
-    quote:
-      "Frontier Restoration LLC is truly exceptional! Josh and his team are punctual and deliver great results. Their attention to detail is impeccable and they communicate every step of the way. I was completely impressed by their professionalism and dedication to quality work. Their outstanding customer service is second to none. Highly recommend!",
-    name: "Robert",
-    location: "Fairbanks, Alaska",
-    service: "Restoration",
-  },
-  {
-    id: 3,
-    quote:
-      'Ross and his team of professionals did an excellent job of cleaning up a "crappy" situation in our crawl space. They had to take the entire vapor barrier out and also haul out a solid 600lbs of stuff we stored in the space. All areas were cleaned professionally. This job took his team of three just four hours. Frontier needs to be the first phone call you make when you discover a leak that has caused damage. This was covered by my homeowners policy.',
-    name: "Eric",
-    location: "Fairbanks, Alaska",
-    service: "Water damage repair & Sewage cleanup",
-  },
-];
 
 export default function Testimonials() {
   return (
---
+++ data/testimonials.js
@@ -0,0 +1,26 @@
+export const testimonials = [
+  {
+    id: 1,
+    quote:
+      "Frontier Restoration came out to our place for a Mold remediation, and they did an amazing job! They also helped us bag up all our clothes and hangers, which was such a huge help. Josh was very friendly and professional, and his worker Bodie was wonderful too. They knocked it out in 30 mins! I highly recommend Frontier Restoration to anyone who needs help with mold cleanup, they made a stressful situation so much easier!",
+    name: "Kelsey",
+    location: "Fairbanks, Alaska",
+    service: "Mold inspection & remediation",
+  },
+  {
+    id: 2,
+    quote:
+      "Frontier Restoration LLC is truly exceptional! Josh and his team are punctual and deliver great results. Their attention to detail is impeccable and they communicate every step of the way. I was completely impressed by their professionalism and dedication to quality work. Their outstanding customer service is second to none. Highly recommend!",
+    name: "Robert",
+    location: "Fairbanks, Alaska",
+    service: "Restoration",
+  },
+  {
+    id: 3,
+    quote:
+      'Ross and his team of professionals did an excellent job of cleaning up a "crappy" situation in our crawl space. They had to take the entire vapor barrier out and also haul out a solid 600lbs of stuff we stored in the space. All areas were cleaned professionally. This job took his team of three just four hours. Frontier needs to be the first phone call you make when you discover a leak that has caused damage. This was covered by my homeowners policy.',
+    name: "Eric",
+    location: "Fairbanks, Alaska",
+    service: "Water damage repair & Sewage cleanup",
+  },
+];
