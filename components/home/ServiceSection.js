656aacf2502d2fad7206af78884e461b184c9a21
David Gray <davidagray2005@gmail.com>
2026-08-04 14:39:12 -0800

    changed the image for mold

--- components/home/ServiceSection.js
+++ components/home/ServiceSection.js
@@ -23,7 +23,7 @@ const services = [
     description:
       "Professional mold removal and moisture control to protect your property.",
     href: "/mold-remediation",
-    image: "/images/services/mold-damage.jpg",
+    image: "/images/services/mold-damage.JPG",
     alt: "Professional mold remediation service",
   },
 
